f7805d61daf967cd6503c7b3df1b567e85a2c24d
David Fernández Puentes <dfernandezpuentes@gmail.com>
2024-06-16 18:20:37 +0200

    Feature-Add realtime updates

--- web/app.py
+++ web/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify, render_template
+from flask_socketio import SocketIO
 import redis
+import eventlet
 
+
+eventlet.monkey_patch()
 app = Flask(__name__)
-
+socketio = SocketIO(app, async_mode='eventlet')
 redis_cache = redis.Redis(host='redis', port='6379', db=0)
 
 @app.route('/prices', methods=['GET'])
@@ -15,5 +19,16 @@ def get_prices():
 def index():
     return render_template('index.html')
 
+@app.route('/realtime')
+def realtime():
+    prices = get_prices()
+    return render_template('realtime.html')
+
+@socketio.on('update')
+def handle_update(data):
+    print('Actualización recibida', data)
+    socketio.emit('update', data, broadcast=True)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    socketio.run(app, host='0.0.0.0', port=5001, allow_unsafe_werkzeug=True)
